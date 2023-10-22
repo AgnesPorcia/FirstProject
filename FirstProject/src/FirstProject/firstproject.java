39637ca06f89b41b3a628674ff5616f841132e96
agnes.porcia <agnes.porcia@CHNSHL564415L.ad.infosys.com>
2023-10-22 06:44:10 +0530

    Program

---
+++ FirstProject/src/FirstProject/firstproject.java
@@ -0,0 +1,11 @@
+package FirstProject;
+
+public class firstproject {
+	String printing;
+	public static void main(String[] args) {
+		firstproject fp=new firstproject();
+		fp.printing="Hello";
+		System.out.println("Text is: " +fp.printing);
+		}
+	}
+
